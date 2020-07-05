75c4cd2718f44c698f5c5de0dd3d0cc70fb0a5a7
Sam Holden <sholden@worth.systems>
2020-07-06 00:25:33 +0100

    fixes for base schema

--- schemas/exercise/max_pull_ups.sql
+++ schemas/exercise/max_pull_ups.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `exercise`.`minsert` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `date` INT NULL,
+  `date` DATETIME NULL,
   `week` INT NULL,
   `workout` INT NULL,
-  `set` INT NULL,
+  `round` INT NULL,
   `reps` INT NULL,
   `weight` INT NULL,
   PRIMARY KEY (`id`),
